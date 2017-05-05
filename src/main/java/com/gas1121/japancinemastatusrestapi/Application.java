25713ca73534f0baab5ff91e5ab8e7f298c44238
gas1121 <jtdzhx@gmail.com>
2017-05-05 19:41:50 +0800

    swagger api doc devleop.

--- src/main/java/com/gas1121/japancinemastatusrestapi/Application.java
+++ src/main/java/com/gas1121/japancinemastatusrestapi/Application.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,7 +29,7 @@ public class Application {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.gas1121.japancinemastatusrestapi.action"))
                 .paths(PathSelectors.any())
                 .build();
     }
